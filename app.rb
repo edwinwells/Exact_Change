3aefe042de112b2fabca1bfee43543e3f414d05a
edwinwells <edwin.wallace@yandex.com>
2017-05-15 23:27:08 -0400

    Tightened-up code.

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
 require 'sinatra'
 require_relative 'exact_change.rb'
+enable :sessions
 
 get '/' do
     erb :User_Input_Amount
 end
-
-post '/exact_change(changevalue)' do
+ 
+post '/exact_change' do
 	number = params[:changevalue]
-	result = exact_change(number)
+	session[:result] = exact_change(number)
 	# "OK, for #{number}, you will need #{result}!  Get to countin'!!"
 	erb :showinfo, :locals => {:number => number, 
-		                       :result => result}
+		                       :result => session[:result]}
 end
